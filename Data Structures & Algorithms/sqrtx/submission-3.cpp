ca72c36239f8830b9cc80389d75343aac361f4cb
shashankjai <157102157+shashankjai@users.noreply.github.com>
2026-08-31 11:52:15 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=0,r=sqrt(x)+5;
+        int ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(1LL*mid*mid<=x){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
